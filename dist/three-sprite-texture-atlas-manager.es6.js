bb6ac7a00f5fa21754b0eaf94ee43179abbd0b21
Lianna Eeftinck <liannaee@gmail.com>
2021-03-14 00:08:58 +0000

    Update generated files

--- dist/three-sprite-texture-atlas-manager.es6.js
+++ dist/three-sprite-texture-atlas-manager.es6.js
@@ -845,5 +845,18 @@ var TextureManager = /*#__PURE__*/function () {
   return TextureManager;
 }();
 
+/**
+The main entry point for 'global' mode, to be used when you're not able to use `require();` or ES6 modules to load the functionality of this library. Include the library by loading the JavaScript directly, or combine it with your other code, and then do:
+
+```javascript
+// Instantiate a new TextureManager with 512x512 textures
+var textureManager = new window.threeSpriteAtlasTextureManager( 512 );
+```
+* @namespace threeSpriteAtlasTextureManager
+* @constructor
+* @global
+* @param {integer} [size=1024] Optional size for the textures. Must be a power of two.
+*/
+
 export default TextureManager;
 //# sourceMappingURL=three-sprite-texture-atlas-manager.es6.js.map
